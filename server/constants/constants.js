5abe17414868c4069009e18bc6ada5531924f07c
sarvesh rajpure <rajpuresarvesh@gmail.com>
2022-03-24 14:03:24 +0530

    Removed server from this repo

--- server/constants/constants.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  APP_VALIDATIONS: {
-    idValidation: { version: "uuidv4" },
-    strongPasswordRegex:
-      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/,
-    phoneRegex: /^[6-9]\d{9}$/,
-  },
-};
