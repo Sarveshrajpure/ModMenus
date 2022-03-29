7f96659a40e7bb34e0edf8208cd03ea5f6907f30
sarvesh rajpure <rajpuresarvesh@gmail.com>
2022-03-30 02:29:18 +0530

    Added nav and footer to login,regiser pages ,Added hero3 component

---
+++ src/Home/components/Hero3.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Hero3.css";
+import dashboardSvg from "../../assests/dashboard.svg";
+import menucard from "../../assests/menucard.svg";
+
+const Hero3 = () => {
+  return (
+    <div className="hero3Wrapper">
+      <div className="hero3block">
+        <div className="hero3Title text-center font-bold text-4xl md:text-5xl lg:text-5xl py-10">
+          Manage, Track & get feedback
+        </div>
+        <div className="hero2CardWrapper  lg:flex lg:justify-between  lg:px-80 md:px-40">
+          <div className="hero3Left flex justify-center ">
+            <img className="w-64" src={dashboardSvg} alt="dashboard" />
+          </div>
+          <div className="hero2Right p-4 ">
+            <div className="hero2pointsCard flex flex justify-center">
+              <div className="pl-6 text-left ">
+                <ol className="list-disc pt-8 pl-5 ">
+                  <li className="pt-2 text-lg ">
+                    Update menu items, pricing & photos instantly.
+                  </li>
+                  <li className="pt-2 text-lg">
+                    Access & utilise customer data
+                  </li>
+                  <li className="pt-2 text-lg">Get customer feedbacks.</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero3;
