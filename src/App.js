cf6203e35403d841be55e995afe2d4b863ddb3c4
sarvesh rajpure <rajpuresarvesh@gmail.com>
2022-04-03 21:23:45 +0530

    Readded menu route , fixed if condition for image in fooditem component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Register from "./Register";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
+import Menu from "./Menu";
 import PrivateRoute from "./Utilities/PrivateRoute";
 import PreventSigninRoute from "./Utilities/preventSignRoute";
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element={<PreventSigninRoute />}>
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/login" element={<Login />} />
+        <Route exact path="/menu/:menuReference" element={<Menu />} />
       </Route>
       <Route path="/" element={<PrivateRoute />}>
         <Route exact path="/dashboard" element={<Dashboard />} />
